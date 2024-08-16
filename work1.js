fd8795b711c007f2865c72b39bdbf78afaf7e370
Martin Brabenec <crazybrab@gmail.com>
2024-08-16 07:49:46 +0200

    ds

--- work1.js
+++ work1.js
@@ -1,13 +1,7 @@
 const printForecast = arr => {
-  for (let i = 0; arr.length; i++) {
-    console.log(`$(i) - $arr[i]`);
-  }
-};
-
-const printForecast = arr => {
   let result = "";
   for (let i = 0; i < arr.length; i++) {
-    result += `in days...${i} - ${arr[i]} `;
+    result += `in 1days...${i} - ${arr[i]} `;
   }
   return result.trim(); // trim to remove any trailing space
 };
